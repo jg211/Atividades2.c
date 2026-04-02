7c36384ba194a7673a2b6e6b03d319109c72dba0
jg211 <jbbarrosantos@gmail.com>
2026-04-02 19:19:34 -0300

    Create exec3.c

---
+++ exec3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main (){
+     float nota;
+
+    printf("Digite a nota: ");
+    scanf("%f", &nota);
+
+    if (nota >= 7) {
+        printf("Aprovado\n");
+    } else if (nota >= 5) {
+        printf("Recuperação\n");
+    } else {
+        printf("Reprovado\n");
+    }
+
+    
+    return 0;
+}
