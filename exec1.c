ef7c23d2192b915ef5185e9c108902a36019d8d3
jg211 <jbbarrosantos@gmail.com>
2026-04-02 19:15:17 -0300

    Create exec1.c

---
+++ exec1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+    int idade;
+    printf("Digite sua idade:\n");
+    scanf("%i", &idade);
+    if(idade >= 18) {
+        printf("Pode entrar.\n");
+    }else if(idade >= 16) {
+        printf("Pode entrar com responsável.\n");
+    }else {
+        printf("Não pode entrar.\n");
+    }
+    
+    return 0;
+}
